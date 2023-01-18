900bca22a5070bc8584bc5930497bc7a814187a3
ibrahimkhalilov <ibrahim.khalilov@yahoo.com>
2023-01-17 20:34:15 -0500

    add views and navigation

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -7,17 +7,24 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DeliMeal'),
       ),
-      children: DUMMY_CATEGORIES
-          .map(
-            (catData) => CategoryItem(catData.title, catData.color),
-          )
-          .toList(),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(catData.title, catData.color),
+            )
+            .toList(),
+      ),
     );
   }
 }
