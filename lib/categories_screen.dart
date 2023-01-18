1c16b96908f34b197fbc3be3b6d700e81b289e1e
ibrahimkhalilov <ibrahim.khalilov@yahoo.com>
2023-01-17 20:39:16 -0500

    pass data via constructor

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -21,7 +21,8 @@ class CategoriesScreen extends StatelessWidget {
         ),
         children: DUMMY_CATEGORIES
             .map(
-              (catData) => CategoryItem(catData.title, catData.color),
+              (catData) =>
+                  CategoryItem(catData.id, catData.title, catData.color),
             )
             .toList(),
       ),
