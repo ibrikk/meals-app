ff0e1e93a6842f5d051d8352204e4df00a94d780
ibrahimkhalilov <ibrahim.khalilov@yahoo.com>
2023-01-21 02:53:18 -0500

    complete navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/category_meals_screen.dart';
 import 'package:meals_app/screens/meal_detail_screen.dart';
@@ -11,30 +13,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.amber,
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        fontFamily: 'Raleway',
-        //   textTheme: ThemeData.light().textTheme.copyWith(
-        //         bodyText1: TextStyle(
-        //           color: Color.fromRGBO(20, 51, 51, 1),
-        //         ),
-        //         bodyText2: TextStyle(
-        //           color: Color.fromRGBO(20, 51, 51, 1),
-        //         ),
-        //         titleLarge:
-        //             TextStyle(fontSize: 24, fontFamily: 'RobotoCondensed'),
-        //       ),
-      ),
-      // home: CategoriesScreen(),
-      initialRoute: '/', // OPTIONAL
-      routes: {
-        '/': (ctx) => CategoriesScreen(),
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
-        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
-      },
-    );
+        title: 'DeliMeals',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          //   textTheme: ThemeData.light().textTheme.copyWith(
+          //         bodyText1: TextStyle(
+          //           color: Color.fromRGBO(20, 51, 51, 1),
+          //         ),
+          //         bodyText2: TextStyle(
+          //           color: Color.fromRGBO(20, 51, 51, 1),
+          //         ),
+          //         titleLarge:
+          //             TextStyle(fontSize: 24, fontFamily: 'RobotoCondensed'),
+          //       ),
+        ),
+        // home: CategoriesScreen(),
+        initialRoute: '/', // OPTIONAL
+        routes: {
+          '/': (ctx) => CategoriesScreen(),
+          CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+          MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        },
+        onUnknownRoute: (settings) {
+          return MaterialPageRoute(
+            builder: (ctx) => CategoriesScreen(),
+          );
+        });
   }
 }
